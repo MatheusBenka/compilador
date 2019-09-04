7fbd5b4fcb57f2fa223043bd9cd18521790b6cf0
MATHEUS HENRIQUE CASAL DO SANTOS BENKA <ALU201617770@uniritter.local>
2019-09-03 21:57:17 -0300

    final da aula agora

--- CompiladorSln/Compilador/Analisador.cs
+++ CompiladorSln/Compilador/Analisador.cs
@@ -18,11 +18,24 @@ namespace Compilador
         public List<Token> Analisar()
         {
             int intch = 0;
+            int linha = 1;
+            int coluna = 0;
             using (FileStream fr = new FileStream(_path,FileMode.Open,FileAccess.ReadWrite))
             {
                 while((intch = fr.ReadByte()) != -1)
                 {
                     char a = (char)intch;
+                    coluna++;
+                    if (Char.IsLetter(a))
+                    {
+                        Console.WriteLine("{0} é letra", a);
+                    }
+
+                    if(a == '\n') // final de linha
+                    {
+                        linha++;
+                        Console.WriteLine("nova linha");
+                    }
                 }
             }
             return this.lt;
